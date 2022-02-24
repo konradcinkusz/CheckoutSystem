5fada62369b966b13a1bff70b5afeb6829b8fe2b
Konrad Cinkusz <konradcinkusz@gmail.com>
2022-02-24 13:56:55 +0100

    add test proj

---
+++ CheckoutSystem.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutSystem.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
